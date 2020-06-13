66408e68887544db0b66ba5bdb7785c38f403bc0
Idrek <iddarg@gmail.com>
2020-06-13 13:31:15 +0200

    New `ValidatorBuilder.ValidateRequired` member

--- src/Builder.fs
+++ src/Builder.fs
@@ -44,6 +44,28 @@ type ValidatorBuilder<'t> () =
             Predicate = predicate
             Rules = List.map (fun rule -> getProperty >> (rule property)) rules
         } :: state
+
+    [<CustomOperation("validateRequired")>]
+    member this.ValidateRequired
+            (
+                state: list<T.State<'t>>,
+                property: string,
+                getProperty: 't -> Option<'p>,
+                rules: list<string -> 'p -> T.Validation>
+            ) : list<T.State<'t>> =
+        {
+            Predicate = fun _ -> true
+            Rules = rules |> List.map (fun rule ->
+                (fun v ->
+                    match getProperty v with
+                    | None -> 
+                        Error <| Set [{ 
+                            T.Message = "Required value in 'Option' type"
+                            T.Property = property
+                            T.Code = "RequiredOption"
+                        }]
+                    | Some p -> rule property p))
+        } :: state
         
 
  
