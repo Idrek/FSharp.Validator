1fc587e9e71488df224eebe22f53c4ebac687bfc
Idrek <iddarg@gmail.com>
2020-06-13 13:40:19 +0200

    New `Api.withValidator` function

--- src/Api.fs
+++ src/Api.fs
@@ -1,6 +1,20 @@
 module Validator.Api
 
 module B = Validator.Builder
+module T = Validator.Types
 
 let validator<'t>() : B.ValidatorBuilder<'t> = B.ValidatorBuilder<'t>()
+
+let withValidator  
+        (validate: 't -> T.Validation)
+        (property: string)
+        (elem: 't) 
+        : T.Validation =
+    match validate elem with
+    | Ok () -> Ok ()
+    | Error invalids -> 
+        invalids 
+        |> Set.map (fun (invalid: T.Invalid) -> 
+            { invalid with Property = sprintf "%s.%s" property invalid.Property })
+        |> Error
 
