3ec82c02f2cd96bdf4b04bfbd449d07ded51c05c
Idrek <iddarg@gmail.com>
2020-06-13 13:29:23 +0200

    New `ValidatorBuilder.ValidateWith` member

--- src/Builder.fs
+++ src/Builder.fs
@@ -6,5 +6,18 @@ type ValidatorBuilder<'t> () =
 
     member this.Yield (_: unit) : list<T.State<'t>> = List.empty
 
+    [<CustomOperation("validateWith")>]
+    member this.ValidateWith 
+            (
+                state: list<T.State<'t>>,
+                property: string,
+                getProperty: 't -> 'p,
+                rules: list<string -> 'p -> T.Validation>
+            ) : list<T.State<'t>> =
+        {
+            Predicate = fun _ -> true
+            Rules = List.map (fun rule -> getProperty >> (rule property)) rules
+        } :: state
+        
 
  
