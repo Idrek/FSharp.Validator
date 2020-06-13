2db1d6453093dd6e1039f1a8d36cf42be88cfb64
Idrek <iddarg@gmail.com>
2020-06-13 13:30:10 +0200

    New `ValidatorBuilder.ValidateBasic` member

--- src/Builder.fs
+++ src/Builder.fs
@@ -18,6 +18,18 @@ type ValidatorBuilder<'t> () =
             Predicate = fun _ -> true
             Rules = List.map (fun rule -> getProperty >> (rule property)) rules
         } :: state
+
+    [<CustomOperation("validateBasic")>]
+    member this.ValidateBasic
+            (
+                state: list<T.State<'t>>,
+                property: string,
+                rules: list<string -> 't -> T.Validation>
+            ) : list<T.State<'t>> =
+        {
+            Predicate = fun _ -> true
+            Rules = List.map (fun rule -> rule property) rules
+        } :: state
         
 
  
