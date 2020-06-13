558b000d6d46724c96bb95b123e91a6a905ea939
Idrek <iddarg@gmail.com>
2020-06-13 13:42:18 +0200

    New `Api.eachWithValidator` function

--- src/Api.fs
+++ src/Api.fs
@@ -18,3 +18,23 @@ let withValidator
             { invalid with Property = sprintf "%s.%s" property invalid.Property })
         |> Error
 
+let eachWithValidator  
+        (validate: 't -> T.Validation)
+        (property: string)
+        (elems: seq<'t>) 
+        : T.Validation =
+    let validatedElems : seq<T.Validation * int> = 
+        Seq.mapi (fun index item -> (validate item, index)) elems
+    let invalids : Set<T.Invalid> = 
+        validatedElems 
+        |> Seq.map (fun validated ->
+            match validated with
+            | (Ok (), _) -> Set.empty
+            | (Error invalids, index) -> 
+                invalids |> Set.map (fun invalid -> 
+                    { invalid with Property = sprintf "%s.[%d].%s" property index invalid.Property }))
+        |> Set.unionMany
+    match Set.count invalids with
+    | 0 -> Ok ()
+    | _ -> Error invalids
+
